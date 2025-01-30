cabeaa9ab65c3db3c6c160ef4ea067b7bb12f588
Ario <ariobarxan.dev@gmail.com>
2025-01-31 08:41:39 +1100

    [feature]: Implement a placeholder view for "ChatsView"

---
+++ AIChat/Core/Chats/ChatsView.swift
@@ -0,0 +1,21 @@
+//
+//  ChatsView.swift
+//  AIChat
+//
+//  Created by Ario Zarepour on 31/1/2025.
+//
+
+import SwiftUI
+
+struct ChatsView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Chats")
+        }
+        .navigationTitle("Chats")
+    }
+}
+
+#Preview {
+    ChatsView()
+}
