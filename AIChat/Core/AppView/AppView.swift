7cd7ab864371571682a6329614edd3a0bbb265e7
Ario <ariobarxan.dev@gmail.com>
2025-01-31 09:49:26 +1100

    [feature]: replace using "AppState" instance instead of directly using "UserDefaults" in View for saving and fetching showTabBar(APPState)

--- AIChat/Core/AppView/AppView.swift
+++ AIChat/Core/AppView/AppView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct AppView: View {
 
-    @AppStorage("showTabbarView") var showTabBar: Bool = false
+    @State var appState: AppState = AppState()
 
     var body: some View {
         AppViewBuilder(
-            showTabBar: showTabBar,
+            showTabBar: appState.showTabBar,
             mainView: {
                 ZStack {
                     Color.red.ignoresSafeArea()
@@ -27,12 +27,13 @@ struct AppView: View {
                 }
             }
         )
+        .environment(appState)
     }
 }
 
 #Preview("AppView - Tabbar") {
-    AppView(showTabBar: true)
+    AppView(appState: AppState(showTabBar: true))
 }
 #Preview("AppView - Onboarding") {
-    AppView(showTabBar: false)
+    AppView(appState: AppState(showTabBar: false))
 }
