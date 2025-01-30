ca4f86c30817838ef6d3d8dbab1cdfc64bae7640
Ario <ariobarxan.dev@gmail.com>
2025-01-31 08:57:43 +1100

    [feature]: implement a placeholder view for "ExploreView"

---
+++ AIChat/Core/Explore/ExploreView.swift
@@ -0,0 +1,21 @@
+//
+//  ExploreView.swift
+//  AIChat
+//
+//  Created by Ario Zarepour on 31/1/2025.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        NavigationStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Explore")
+        }
+    }
+}
+
+#Preview {
+    ExploreView()
+}
