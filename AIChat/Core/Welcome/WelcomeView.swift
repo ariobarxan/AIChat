085d0c82c1ada6c54bf5522a1cd2dd2eba4e0162
Ario <ariobarxan.dev@gmail.com>
2025-01-31 09:32:17 +1100

    [feature]: implement on boarding completed link in "WelcomeView"

--- AIChat/Core/Welcome/WelcomeView.swift
+++ AIChat/Core/Welcome/WelcomeView.swift
@@ -10,7 +10,21 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         NavigationStack {
-            Text("Welcome!")
+            VStack {
+                Text("Welcome!")
+                    .frame(maxHeight: .infinity)
+
+                NavigationLink(destination: {Text("OnBoarding Completed")}) {
+                    Text("Get Started")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 55)
+                        .background(.accent)
+                        .cornerRadius(16)
+                }
+            }
+            .padding()
         }
     }
 }
