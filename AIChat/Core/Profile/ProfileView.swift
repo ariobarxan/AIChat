f74e60fa48b35fbcb86db103a8d0cc529c4f1494
Ario <ariobarxan.dev@gmail.com>
2025-01-31 09:10:34 +1100

    [feature]: implement showing "SettingsView" from "ProfileView"

--- AIChat/Core/Profile/ProfileView.swift
+++ AIChat/Core/Profile/ProfileView.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var showSettings: Bool = false
+
     var body: some View {
         NavigationStack {
             Text("Profile")
                 .navigationTitle("Profile")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        settingButton
+                    }
+                }
         }
+        .sheet(isPresented: $showSettings) {
+            Text("Settings View")
+        }
+    }
+
+    private var settingButton: some View {
+        Button(action: onSettingButtonPressed) {
+            Image(systemName: "gear")
+                .font(.headline)
+        }
+    }
+
+    private func onSettingButtonPressed() {
+        showSettings = true
     }
 }
 
