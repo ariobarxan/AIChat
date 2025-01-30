ab5b8ac2736c077dfabada785dcfc8359074d580
Ario <ariobarxan.dev@gmail.com>
2025-01-31 08:59:08 +1100

    [feature]: implement a placeholder for "ProfileView"

---
+++ AIChat/Core/Profile/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  AIChat
+//
+//  Created by Ario Zarepour on 31/1/2025.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Profile")
+                .navigationTitle("Profile")
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
