785c65fb1147a826116b03d9179d1b54130af053
Ario <ariobarxan.dev@gmail.com>
2025-01-31 09:00:22 +1100

    [feature]: implement a placeholder view for "WelcomeView"

---
+++ AIChat/Core/Welcome/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  AIChat
+//
+//  Created by Ario Zarepour on 31/1/2025.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        NavigationStack {
+            Text("Welcome!")
+        }
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
