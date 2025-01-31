95b550e88393aa7dd72bfa4ef410ba857514a14a
Ario <ariobarxan.dev@gmail.com>
2025-01-31 16:21:29 +1100

    [feature]: implement addingTimeInterval with days, hours and minutes

---
+++ AIChat/Services/Chat/Models/ChatMessageModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChatMessageModel.swift
+//  AIChat
+//
+//  Created by Ario Zarepour on 31/1/2025.
+//
+
+import Foundation
---
+++ AIChat/Services/Chat/Models/ChatModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChatModel.swift
+//  AIChat
+//
+//  Created by Ario Zarepour on 31/1/2025.
+//
+
+import Foundation
