bf8d57173e960b62ff219d1d947f1a8e66680a11
Ario <ariobarxan.dev@gmail.com>
2025-01-31 11:34:26 +1100

    [feature]: implement "WelcomeView" page UI

--- AIChat/Core/Welcome/WelcomeView.swift
+++ AIChat/Core/Welcome/WelcomeView.swift
@@ -8,18 +8,69 @@
 import SwiftUI
 
 struct WelcomeView: View {
+
+    @State private var imageName: String = Constants.randomImage
+
     var body: some View {
         NavigationStack {
-            VStack {
-                Text("Welcome!")
-                    .frame(maxHeight: .infinity)
+            VStack(spacing: 8) {
+                ImageLoaderView(urlString: imageName)
+                    .ignoresSafeArea()
 
-                NavigationLink(destination: OnboardingCompletedView()) {
-                    Text("Get Started")
-                        .callToActionButtonStyle()
-                }
+                titleSection
+                    .padding(.top, 24)
+
+                ctaButtons
+                    .padding()
+
+                policyLinks
             }
-            .padding()
+        }
+    }
+
+    private var titleSection: some View {
+        VStack(spacing: 8) {
+            Text("AI Chat 🤙")
+                .font(.largeTitle)
+                .bold()
+
+            Text("Medium @ Ariobarxan")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+        }
+    }
+
+    private var ctaButtons: some View {
+        VStack(spacing: 8) {
+            NavigationLink(destination: OnboardingCompletedView()) {
+                Text("Get Started")
+                    .callToActionButtonStyle()
+            }
+
+            Text("Already have an account? Sign in!")
+                .underline()
+                .font(.body)
+                .padding(8)
+                .background(Color.black.opacity(0.001))
+                .onTapGesture {
+                    // TODO: Implement logic
+                }
+        }
+    }
+
+    private var policyLinks: some View {
+        HStack(spacing: 8) {
+            Link(destination: URL(string: Constants.termsOfServiceUrl)!) {
+                Text("Terms of Service")
+            }
+
+            Circle()
+                .fill(.accent)
+                .frame(width: 4, height: 4)
+
+            Link(destination: URL(string: Constants.privacyPolicyUrl)!) {
+                Text("Privacy Policy")
+            }
         }
     }
 }
