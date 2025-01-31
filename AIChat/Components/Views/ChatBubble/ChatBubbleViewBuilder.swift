a4c55817469a0823648d74e43465eb6894629afc
Ario <ariobarxan.dev@gmail.com>
2025-01-31 21:16:54 +1100

    [feature]: implement "ChatBubbleViewBuilder"

--- AIChat/Components/Views/ChatBubble/ChatBubbleViewBuilder.swift
+++ AIChat/Components/Views/ChatBubble/ChatBubbleViewBuilder.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct ChatBubbleViewBuilder: View {
+
+    var message: ChatMessageModel = .mock
+    var isCurrentUser: Bool = false
+    var imageName: String?
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ChatBubbleView(
+            text: message.content ?? "",
+            textColor: isCurrentUser ? .white : .primary,
+            backgroundColor: isCurrentUser ? .accent : Color(uiColor: .systemGray6),
+            showImage: !isCurrentUser,
+            imageName: imageName
+        )
+        .frame(maxWidth: .infinity, alignment: isCurrentUser ? .trailing : .leading)
+        .padding(.leading, isCurrentUser ? 75 : 0)
+        .padding(.trailing, isCurrentUser ? 0 : 75)
     }
 }
 
 #Preview {
-    ChatBubbleViewBuilder()
+    ScrollView {
+        VStack(spacing: 24) {
+            ChatBubbleViewBuilder()
+            ChatBubbleViewBuilder(isCurrentUser: true)
+            ChatBubbleViewBuilder(
+                message: ChatMessageModel(
+                    id: UUID().uuidString,
+                    chatId: UUID().uuidString,
+                    authorId: UUID().uuidString,
+                    content: "This is some longer content that goes on to multiple lines and keeps on going to another line!",
+                    seenByIds: nil,
+                    dateCreated: .now
+                )
+            )
+            ChatBubbleViewBuilder(
+                message: ChatMessageModel(
+                    id: UUID().uuidString,
+                    chatId: UUID().uuidString,
+                    authorId: UUID().uuidString,
+                    content: "This is some longer content that goes on to multiple lines and keeps on going to another line!",
+                    seenByIds: nil,
+                    dateCreated: .now
+                ),
+                isCurrentUser: true
+            )
+        }
+        .padding(12)
+    }
 }
