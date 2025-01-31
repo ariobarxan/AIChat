fe6d082968785022f807f3d1dc50f76de2fe335e
Ario <ariobarxan.dev@gmail.com>
2025-01-31 12:06:24 +1100

    [feature]: implement "OnboardingCompletedView" UI

--- AIChat/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
+++ AIChat/Core/Onboarding/CompletedView/OnboardingCompletedView.swift
@@ -9,23 +9,57 @@ import SwiftUI
 
 struct OnboardingCompletedView: View {
     @Environment(AppState.self) private var appState
+    @State private var isCompletingProfileSetup: Bool = false
+    private var selectedColor: Color = .orange
+
 
     var body: some View {
-        VStack {
-            Text("Onboarding Completed")
-                .frame(maxHeight: .infinity)
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Setup complete!")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(selectedColor)
 
-            Button(action: onFinishedButtonPressed) {
-                Text("Finish")
-                    .callToActionButtonStyle()
-            }
+
+            Text("We've set up your profile and you're ready to start chatting.")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundStyle(.secondary)
         }
-        .padding()
+        .frame(maxHeight: .infinity)
+        .safeAreaInset(edge: .bottom, content: {
+            ctaButton
+        })
+        .padding(24)
+        .toolbar(.hidden, for: .navigationBar)
     }
 
     private func onFinishedButtonPressed() {
+        isCompletingProfileSetup = true
+
         // TODO: logic to complete onboarding
-        appState.updateViewState(showTabBarView: true)
+        Task {
+            try await Task.sleep(for: .seconds(3))
+            isCompletingProfileSetup = false
+//            try await saveUserProfile(color: selectedColor)
+
+            appState.updateViewState(showTabBarView: true)
+        }
+    }
+
+    private var ctaButton: some View {
+        Button(action: onFinishedButtonPressed) {
+            ZStack {
+                if isCompletingProfileSetup {
+                    ProgressView()
+                        .tint(.white)
+                } else {
+                    Text("Finish")
+                }
+            }
+            .callToActionButtonStyle()
+        }
+        .disabled(isCompletingProfileSetup)
     }
 }
 
