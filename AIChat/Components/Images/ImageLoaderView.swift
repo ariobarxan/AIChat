fdfb22434b6a1fa6672fa0704ac77e0002b7f371
Ario <ariobarxan.dev@gmail.com>
2025-01-31 11:16:24 +1100

    [feature]: implement ImageLoaderView

---
+++ AIChat/Components/Images/ImageLoaderView.swift
@@ -0,0 +1,32 @@
+//
+//  ImageLoaderView.swift
+//  AIChat
+//
+//  Created by Ario Zarepour on 31/1/2025.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ImageLoaderView: View {
+    var urlString: String = "https://picsum.photos/600/600"
+    var resizingMode: ContentMode = .fill
+
+    var body: some View {
+        Rectangle()
+            .opacity(0)
+            .overlay {
+                WebImage(url: URL(string: urlString))
+                    .resizable()
+                    .indicator(.activity)
+                    .aspectRatio(contentMode: resizingMode)
+                    .allowsHitTesting(false)
+            }
+            .clipped()
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+        .frame(width: 100, height: 200)
+}
