8bcd99bdab3eb486edf4f8836603f08f169363e5
Ario <ariobarxan.dev@gmail.com>
2025-01-31 21:18:52 +1100

    [feature]: implement "ChatView"

--- AIChat/Core/Chat/ChatView.swift
+++ AIChat/Core/Chat/ChatView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct ChatView: View {
+
+    @State private var chatMessages: [ChatMessageModel] = ChatMessageModel.mocks
+    @State private var avatar: AvatarModel? = .mock
+    @State private var currentUser: UserModel? = .mock
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            ScrollView {
+                LazyVStack(spacing: 24) {
+                    ForEach(chatMessages) { message in
+                        messageView(message)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(8)
+            }
+
+            Rectangle()
+                .frame(height: 50)
+        }
+        .navigationTitle(avatar?.name ?? "Chat")
+        .toolbarTitleDisplayMode(.inline)
+    }
+
+    private func messageView(_ message: ChatMessageModel) -> some View {
+        let isCurrentUser = message.authorId == currentUser?.userId
+        return ChatBubbleViewBuilder(
+            message: message,
+            isCurrentUser: isCurrentUser,
+            imageName: isCurrentUser ? nil : avatar?.profileImageName
+        )
     }
 }
 
 #Preview {
-    ChatView()
+    NavigationStack {
+        ChatView()
+    }
 }
