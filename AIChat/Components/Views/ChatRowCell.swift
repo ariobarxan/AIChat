0d7d76dae6bea3d4e527bc8e937256f85e54bd4f
Ario <ariobarxan.dev@gmail.com>
2025-01-31 16:48:33 +1100

    [feature]: implement "ChatRowCell"

--- AIChat/Components/Views/ChatRowCell.swift
+++ AIChat/Components/Views/ChatRowCell.swift
@@ -5,4 +5,86 @@
 //  Created by Ario Zarepour on 31/1/2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct ChatRowCell: View {
+
+    var imageName: String? = Constants.randomImage
+    var headline: String? = "Alpha"
+    var subheadline: String? = "This is the last message in the chat."
+    var hasNewChat: Bool = true
+
+    var body: some View {
+        HStack(spacing: 8) {
+            imageSection
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+
+            textSection
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+            if hasNewChat {
+                newBadge
+            }
+        }
+        .padding(.vertical, 12)
+        .padding(.horizontal, 8)
+        .background(Color(uiColor: UIColor.systemBackground))
+    }
+
+    private var imageSection: some View {
+        ZStack {
+            if let imageName {
+                ImageLoaderView(urlString: imageName)
+            } else {
+                Rectangle()
+                    .fill(.secondary.opacity(0.5))
+            }
+        }
+
+    }
+
+    private var textSection: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            if let headline {
+                Text(headline)
+                    .font(.headline)
+            }
+            if let subheadline {
+                Text(subheadline)
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+            }
+        }
+    }
+
+    private var newBadge: some View {
+        Text("NEW")
+            .font(.caption)
+            .bold()
+            .foregroundStyle(Color.white)
+            .padding(.horizontal, 8)
+            .padding(.vertical, 6)
+            .background(Color.blue)
+            .cornerRadius(6)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.gray.ignoresSafeArea()
+
+        List {
+            ChatRowCell()
+                .removeListRowFormatting()
+            ChatRowCell(hasNewChat: false)
+                .removeListRowFormatting()
+            ChatRowCell(imageName: nil)
+                .removeListRowFormatting()
+            ChatRowCell(headline: nil, hasNewChat: false)
+                .removeListRowFormatting()
+            ChatRowCell(subheadline: nil, hasNewChat: false)
+                .removeListRowFormatting()
+        }
+    }
+}
--- AIChat/Components/Views/ChatRowCellBuilder.swift
+++ AIChat/Components/Views/ChatRowCellBuilder.swift
@@ -8,11 +8,83 @@
 import SwiftUI
 
 struct ChatRowCellBuilder: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+    var currentUserId: String? = ""
+    var chat: ChatModel = .mock
+    var getAvatar: () async -> AvatarModel?
+    var getLastChatMessage: () async -> ChatMessageModel?
+
+    @State private var avatar: AvatarModel?
+    @State private var lastChatMessage: ChatMessageModel?
+
+    @State private var didLoadAvatar: Bool = false
+    @State private var didLoadChatMessage: Bool = false
+
+    private var isLoading: Bool {
+        if didLoadAvatar && didLoadChatMessage {
+            return false
+        }
+
+        return true
     }
+
+    private var hasNewChat: Bool {
+        guard let lastChatMessage, let currentUserId else { return false }
+        return lastChatMessage.hasBeenSeenBy(userId: currentUserId)
+    }
+
+    private var subheadline: String? {
+        if isLoading {
+            return "xxxx xxxx xxxxx xxxx"
+        }
+
+        if avatar == nil && lastChatMessage == nil {
+            return "Error"
+        }
+
+        return lastChatMessage?.content
+    }
+
+    var body: some View {
+        ChatRowCell(
+            imageName: avatar?.profileImageName,
+            headline: isLoading ? "xxxx xxxx" : avatar?.name,
+            subheadline: subheadline,
+            hasNewChat: isLoading ? false : hasNewChat
+        )
+        .redacted(reason: isLoading ? .placeholder : [])
+        .task {
+            avatar = await getAvatar()
+            didLoadAvatar = true
+        }
+        .task {
+            lastChatMessage = await getLastChatMessage()
+            didLoadChatMessage = true
+        }
+    }
+
 }
 
 #Preview {
-    ChatRowCellBuilder()
+    VStack {
+        ChatRowCellBuilder(chat: .mock, getAvatar: {
+            try? await Task.sleep(for: .seconds(5))
+            return .mock
+        }, getLastChatMessage: {
+            try? await Task.sleep(for: .seconds(5))
+            return .mock
+        })
+
+        ChatRowCellBuilder(chat: .mock, getAvatar: {
+            .mock
+        }, getLastChatMessage: {
+            .mock
+        })
+
+        ChatRowCellBuilder(chat: .mock, getAvatar: {
+            nil
+        }, getLastChatMessage: {
+            nil
+        })
+    }
 }
