be4a1525831a5e84eab894d2c25a053dfa170ad0
Ario <ariobarxan.dev@gmail.com>
2025-05-27 20:58:38 +1000

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Ario Liyan on 27/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
